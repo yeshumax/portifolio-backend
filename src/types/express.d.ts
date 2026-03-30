a6a239cfc0496c6cec55f033f4fce0617fb0d3ac
Yeshwond Lisanwork <github.com/yeshumax>
2026-03-30 20:41:36 +0300

    types: add TypeScript type definitions and interfaces

---
+++ src/types/express.d.ts
@@ -0,0 +1,12 @@
+import { Request } from 'express';
+import { IUser } from '../models/User';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser;
+    }
+  }
+}
+
+export {};
